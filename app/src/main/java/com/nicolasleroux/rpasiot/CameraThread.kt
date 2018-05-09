ee0db1be87a4135e0cde6de01c7e5a44d39d2856
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-09 23:32:31 +0200

    feat(Camera Thread): Add a camera thread class to handle the thread

---
+++ app/src/main/java/com/nicolasleroux/rpasiot/CameraThread.kt
@@ -0,0 +1,30 @@
+package com.nicolasleroux.rpasiot
+
+import android.os.Handler
+import android.os.HandlerThread
+
+class CameraThread {
+
+    var thread: HandlerThread? = null
+
+    var handler: Handler? = null
+
+    fun start() {
+        thread = HandlerThread("CameraThread")
+        thread!!.start()
+
+        handler = Handler(thread!!.looper)
+    }
+
+    fun stop() {
+        thread!!.quitSafely()
+
+        try {
+            thread!!.join()
+            thread = null
+            handler = null
+        } catch (e: InterruptedException) {
+            throw Exception("Something went wrong during the safely thread quitting ! $e")
+        }
+    }
+}
