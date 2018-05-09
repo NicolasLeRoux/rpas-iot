e7927ed8d7ade1b07e28843f24610cc52d9654c3
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-09 22:58:24 +0200

    feat(MainActivity): Add method to get the camera manager

--- app/src/main/java/com/nicolasleroux/rpasiot/MainActivity.kt
+++ app/src/main/java/com/nicolasleroux/rpasiot/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nicolasleroux.rpasiot
 
 import android.app.Activity
+import android.content.Context
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 
 private val TAG = MainActivity::class.java.simpleName
@@ -15,4 +17,12 @@ class MainActivity : Activity() {
         super.onDestroy()
     }
 
+    /**
+     * Method to get the CameraManager from the MainActivity context.
+     */
+    private fun cameraManager(): CameraManager {
+        val ctx: Context = this.applicationContext
+
+        return ctx.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
 }
