597757896029c51965ba7ab06db5faa8bec707c7
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-10 17:13:55 +0200

    feat(Capture Session Callback): Create class to handle capture callback

---
+++ app/src/main/java/com/nicolasleroux/rpasiot/CameraCaptureSessionStateCallback.kt
@@ -0,0 +1,13 @@
+package com.nicolasleroux.rpasiot
+
+import android.hardware.camera2.CameraCaptureSession
+
+class CameraCaptureSessionStateCallback: CameraCaptureSession.StateCallback() {
+    override fun onConfigureFailed(session: CameraCaptureSession?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onConfigured(session: CameraCaptureSession?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
