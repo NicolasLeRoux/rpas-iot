8f2c92224116eb4b1b7435e50e0cdb3567dcc720
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-10 12:42:59 +0200

    feat(ImageAvailableCallback): Create callback for camera image

---
+++ app/src/main/java/com/nicolasleroux/rpasiot/ImageAvailableCallback.kt
@@ -0,0 +1,9 @@
+package com.nicolasleroux.rpasiot
+
+import android.media.ImageReader
+
+class  ImageAvailableCallback : ImageReader.OnImageAvailableListener {
+    override fun onImageAvailable(reader: ImageReader?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
