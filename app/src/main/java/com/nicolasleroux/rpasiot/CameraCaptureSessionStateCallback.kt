075f51b75909716ba379b85b4e3536bc562cb477
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-10 17:41:43 +0200

    feat(Camera Capture Session): Set reptiting capture request

--- app/src/main/java/com/nicolasleroux/rpasiot/CameraCaptureSessionStateCallback.kt
+++ app/src/main/java/com/nicolasleroux/rpasiot/CameraCaptureSessionStateCallback.kt
@@ -1,16 +1,27 @@
 package com.nicolasleroux.rpasiot
 
 import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CaptureRequest
 import android.util.Log
 
 private val TAG = CameraCaptureSessionStateCallback::class.java.simpleName
 
-class CameraCaptureSessionStateCallback: CameraCaptureSession.StateCallback() {
+class CameraCaptureSessionStateCallback(val camera: CameraDevice): CameraCaptureSession.StateCallback() {
     override fun onConfigureFailed(session: CameraCaptureSession?) {
         Log.i(TAG, "Camera session configuration failed !")
     }
 
     override fun onConfigured(session: CameraCaptureSession?) {
         Log.i(TAG, "Camera is now configured !")
+
+        val captureBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_RECORD)
+        captureBuilder.addTarget(CameraImageReader.instance().surface())
+        captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+
+        session!!.setRepeatingRequest(captureBuilder.build(),
+                CameraCaptureSessionCaptureCallback(),
+                CameraThreadWrapper.instance().handler)
     }
 }
