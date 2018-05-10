6fae3da2bd7709e0e5e010d660c7dddd67e4489f
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-05-10 17:19:49 +0200

    feat(Capture Session Callback): Add log to track steps

--- app/src/main/java/com/nicolasleroux/rpasiot/CameraCaptureSessionStateCallback.kt
+++ app/src/main/java/com/nicolasleroux/rpasiot/CameraCaptureSessionStateCallback.kt
@@ -1,13 +1,16 @@
 package com.nicolasleroux.rpasiot
 
 import android.hardware.camera2.CameraCaptureSession
+import android.util.Log
+
+private val TAG = CameraCaptureSessionStateCallback::class.java.simpleName
 
 class CameraCaptureSessionStateCallback: CameraCaptureSession.StateCallback() {
     override fun onConfigureFailed(session: CameraCaptureSession?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.i(TAG, "Camera session configuration failed !")
     }
 
     override fun onConfigured(session: CameraCaptureSession?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.i(TAG, "Camera is now configured !")
     }
 }
